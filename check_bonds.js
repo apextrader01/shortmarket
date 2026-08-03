38455bfc740e122f55eaaeb428e48bf035361e54
apextrader01 <appwebsitetester@gmail.com>
2026-08-03 13:54:04 +0530

    chore: Remove leftover junk/temp files from repo

--- check_bonds.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('C:/Users/h4har/Documents/shortmarket/backend/database/stocks.json', 'utf8'));
-const keys = Object.keys(data);
-console.log(JSON.stringify(data[keys[50]], null, 2));
--- test_redis.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('redis');
-const client = createClient();
-client.connect().catch(e => console.log('Connect error', e));
-try {
-    client.subscribe('test', () => {}).catch(e => console.log('Subscribe error:', e));
-} catch(e) {
-    console.log('Sync error:', e);
-}
